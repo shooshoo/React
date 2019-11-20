dd14498490e1df54a14b9eb7880fd48ca00731a2
shilan_hk <shilan_hk@yahoo.com>
2019-11-20 12:51:17 +0330

    before assignment of week 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle
-} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({ dish, onClick }) {
-    return (<Card key={dish.id}>
-        <CardImg width="100%" src={dish.image} alt={dish.name} />
-        <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
-    </Card>)
-}
+    return (
+        <Card >
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>)
+};
 
 // below code is another way of defining a function and is same as function Menu({props})
 
@@ -18,15 +20,27 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         );
     });
     return (
         <div className="container">
             <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
                 {menu}
             </div>
+
         </div>
 
     );
